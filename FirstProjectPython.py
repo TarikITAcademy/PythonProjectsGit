10409b62eb1747fdecec53efdd1f18931a662570
Tarik Bulic <ArchaGen888@gmail.com>
2025-02-14 22:49:51 +0100

    My first Python Project, featuring a topic of "YouTube Channel Name"
    Over the past few days, I had started learning Python, so it's a new Programming Language for me, be gentle!

---
+++ FirstProjectPython.py
@@ -0,0 +1,19 @@
+# Greetings and Introduction
+print("Hello World! Today, we're going to be trying our my first program!")
+print("Let's generate...a YouTube Channel name together? Yes! Let's do it!")
+print("First I need You to think of a theme, something that will be on the Spotlight of Your channel, but try to make it a verb rather than a noun...")
+print("...You got it? Great!")
+# First Input
+theme = input("What is the Theme of Your channel?\n")
+print("Oh?! Good choice! Now think of something that is the co-star of the channel. It can be Your name, or a fake one?")
+print("...It can be a favorite animal or an object You cherish.")
+# Second Input
+co_star = input("What, or who, is the Co-Star of Your channel?\n")
+print("Now let's combine them!")
+# Combination of Inputs
+channel_name = co_star + " " + theme
+# Final Output
+print(channel_name)
+print("Oh that is an amazing name")
+print("It certainly has a ring to it! I wish You the best of luck on Your creative journey through YouTube! Ta-Ta")
+# End
